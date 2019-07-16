2c71aae6362b081ca13e6faaea245bb736b0b106
edwinsanch33 <edwinsanch33@gmail.com>
2019-07-16 09:44:14 -0400

    Finished making detailed page and updated server.js

--- client/src/Components/Detailed.js
+++ client/src/Components/Detailed.js
@@ -2,42 +2,40 @@ import React from 'react'
 import axios from 'axios'
 
 class Detailed extends React.Component {
-  state = { drinks: [] }
+  state = { drink: {} }
+
+  showIngredients = () => {
+    let { drink } = this.state
+    let list = []
+    for( let i = 1; i < 16; i++){
+      if( drink["strMeasure" + i].trim() && drink["strIngredient" + i].trim() ){
+        list.push( <li>{ `${drink["strMeasure" + i]} of ${drink["strIngredient" + i]}` }</li> )
+      }
+    }
+    return list
+  }
 
   componentDidMount(){
-    axios.get(`api/search/${this.props.match.params.idDrink}`)
-      .then(response => this.setState({ drinks: response.data}))
+    axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11021`)
+      .then(response => this.setState({ drink: response.data.drinks[0] || {} }))
   }
+
   render(){
-    const { drinks } = this.state
+    const { drink } = this.state
     return(
-      <React.Fragment>
-       { <img height="300px" src={drinks.strDrinkThumb} alt="fancy-drink"/> } 
-        <h3>{drinks.strDrink}</h3> {/*Drink name */}
+      <div id="detailed">
+        <img  height="200px" src={drink.strDrinkThumb} alt="fancydrink"/>
+        <h3>{drink.strDrink}</h3> {/*Drink name */}
         <hr/>
-        <p>{drinks.strGlass}</p> {/*Drink glass */}
+        <p>{drink.strGlass}</p> {/*Drink glass */}
         <hr/>
-        {<li>{drinks.strMeasure1 && drinks.strIngredient1}</li>}
-        {<li>{drinks.strMeasure2 && drinks.strIngredient2}</li>}
-        {<li>{drinks.strMeasure3 && drinks.strIngredient3}</li>}
-        {<li>{drinks.strMeasure4 && drinks.strIngredient4}</li>}
-        {<li>{drinks.strMeasure5 && drinks.strIngredient5}</li>}
-        {<li>{drinks.strMeasure6 && drinks.strIngredient6}</li>}
-        {<li>{drinks.strMeasure7 && drinks.strIngredient7}</li>}
-        {<li>{drinks.strMeasure8 && drinks.strIngredient8}</li>}
-        {<li>{drinks.strMeasure9 && drinks.strIngredient9}</li>}
-        {<li>{drinks.strMeasure10 && drinks.strIngredient10}</li>}
-        {<li>{drinks.strMeasure11 && drinks.strIngredient11}</li>}
-        {<li>{drinks.strMeasure12 && drinks.strIngredient12}</li>}
-        {<li>{drinks.strMeasure13 && drinks.strIngredient13}</li>}
-        {<li>{drinks.strMeasure14 && drinks.strIngredient14}</li>}
-        {<li>{drinks.strMeasure15 && drinks.strIngredient15}</li>}
+        { drink.idDrink && this.showIngredients() }
         {/* Map of the drink will return <li>{drink measurement} {matching drink ingredient}</li> */}
         <hr/>
-        <p>{drinks.strInstructions}</p> {/*Drink instructions */}
+        <p>{drink.strInstructions}</p> {/*Drink instructions */}
         <hr/>
         <h4>Enjoy!</h4>
-      </React.Fragment>
+      </div>
     )
   }
 }
