c2a58db40c121ae9dda8eadaabfedb03515aa296
sophia banda <sophia.m.banda@gmail.com>
2019-07-17 14:30:59 -0400

    Removed unnecessary code, fixed Browswer Router routs

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,22 @@ import React from "react";
 import "./App.css";
 import DrinkForm from "./Components/DrinkForm";
 import Drinks from "./Components/Drinks";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Detailed from './Components/Detailed';
 import Navbar from './Components/Navbar'
 
-class App extends React.Component {
-  state = { serverMessage: "" };
 
-  // componentDidMount() {
-  //   fetch("/api/demo")
-  //     .then(response => response.json())
-  //     .then(data => this.setState({ serverMessage: data.message }));
-  // }
+class App extends React.Component {
 
   render() {
     return (
       <BrowserRouter>
       <Navbar/>
-          <Route path="/" component={DrinkForm}/>
-          <Route path="/AllDrinks" component={Drinks}/>
+        <Switch>
+          <Route exact path="/" component={DrinkForm}/>
+          <Route exact path="/AllDrinks" component={Drinks}/>
           <Route path="/Detailed/:id" component={Detailed}/>
+        </Switch>
       </BrowserRouter>
     );
   }
